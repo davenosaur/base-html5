440d3d1a33feb270141a1d4cc469d3c0616a58e6
david.murillo <david.murillo@interalia.net>
2014-10-20 17:33:57 -0500

    cambios

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp'),
+ sass = require('gulp-sass'),
   connect = require('gulp-connect');
+
 
 gulp.task('connect', function() {
   connect.server();
 });
 
-gulp.task('default', ['connect']);
+
+
+gulp.task('default', ['sass', 'connect']);
